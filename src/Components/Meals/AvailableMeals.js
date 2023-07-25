e8dde32b98784d46c4abce844c2034d72220840a
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-07-25 14:39:55 +0530

    Making the items visible

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,19 +1,12 @@
 import "./AvailableMeals.css";
-
-const DUMMY_MEALS = [
-	{ id: "m1", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
-	{ id: "m2", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
-	{ id: "m3", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
-	{ id: "m4", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
-];
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = (props) => {
-	const mealsList = DUMMY_MEALS.map((meal) => {
-	return	<li key = {meal.id}>{meal.name}</li>;
-	});
 	return (
-		<section>
-			<ul>{mealsList}</ul>
+		<section className="mealList">
+			<ul>
+				<MealItem />
+			</ul>
 		</section>
 	);
 };
---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./MealItem.css";
+const DUMMY_MEALS = [
+	{ id: "m1", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
+	{ id: "m2", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
+	{ id: "m3", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
+	{ id: "m4", name: "Sushi", description: "Finest fish and veggies", price: 22.99 },
+];
+const MealItem = () => {
+	const mealsList = DUMMY_MEALS.map((meal) => {
+		return (
+			<li key={meal.id}>
+				<span className="name">{meal.name}</span>
+				<span className="description">{meal.description}</span>
+				<span className="price">${meal.price}</span>
+			</li>
+		);
+	});
+	return <React.Fragment>{mealsList}</React.Fragment>;
+};
+
+export default MealItem;
